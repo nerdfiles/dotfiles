c407dd8959c2f0076affbd3719dc08e803b2b05c
Aaron Alexander <nerdfiles@gmail.com>
2011-11-09 03:21:30 -0600

    cleanup

--- configure_git.sh
+++ configure_git.sh
@@ -21,3 +21,27 @@ git config commit.template '/etc/git-commit-template'
 
 git config format.pretty oneline
 
+# == EDITOR ======================================= #
+
+git config --global core.editor 'open -a /Applications/MacVim/MacVim.app/Contents/MacOS/MacVim'
+git config --global diff.tool '/Applications/DiffMerge.app/Contents/MacOS/DiffMerge $LOCAL $REMOTE'
+git config --global merge.tool '/Applications/DiffMerge.app/Contents/MacOS/DiffMerge'
+
+# == MORE ALIASES ======================================= #
+
+git config --global alias.st status
+git config --global alias.ci commit
+git config --global alias.co checkout
+git config --global alias.br branch
+
+# == IGGY ======================================= #
+
+echo ".DS_Store" >> ~/.gitignore
+git config --global core.excludesfile ~/.gitignore
+
+# == BASH COMPLETION ======================================= #
+
+#cp /opt/local/etc/bash_completion.d/git ~/.git-bash-completion.sh
+#echo "[ -f ~/.git-bash-completion.sh ] && . ~/.git-bash-completion.sh" >> ~/.bash_profile
+#. ~/.bash_profile
+
